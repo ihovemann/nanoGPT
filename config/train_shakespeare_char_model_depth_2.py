07685eb85cf7d62d9f67279f300559dd3eb593ac
Rabia Ciftci <145599083+rabiaciftci@users.noreply.github.com>
2026-04-16 14:22:08 +0200

    model-depth(#3)
    Co-authored-by: Rabia Çiftçi <rabiaciftci@ac160786.funky.uni-osnabrueck.de>

---
+++ config/train_shakespeare_char_model_depth_2.py
@@ -0,0 +1,26 @@
+# Baseline configuration
+out_dir = 'out-shakespeare-char_model_depth_2'
+eval_interval = 250
+eval_iters = 200
+log_interval = 10
+always_save_checkpoint = False
+wandb_log = False
+wandb_project = 'nanoGPT-assignment'
+wandb_run_name = 'depth-2'
+dataset = 'shakespeare_char'
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 256
+n_layer = 2
+n_head = 6
+n_embd = 384
+dropout = 0.2
+learning_rate = 1e-3
+max_iters = 5000
+lr_decay_iters = 5000
+min_lr = 1e-4
+beta2 = 0.99
+warmup_iters = 100
+weight_decay = 1e-1
+device = 'cuda' # "mps" for mac, "cuda" for windows, if you have a GPU
+compile = False # set True only on Linux with GPU
